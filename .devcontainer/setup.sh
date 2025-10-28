9afabd6a0a02baa00ee2d00f19f200462003fe4b
workroomprds <jdl@workroom-productions.com>
2025-10-28 15:59:33 +0000

    Modify setup.sh for package installation with sudo
    Updated package installation commands to use sudo and changed build-base to build-essential.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,8 +2,11 @@
 set -eu
 
 # Minimal apk update + essential packages (keeps image small)
-apt update
-apt add --no-cache bash git curl nodejs npm mocha chai nodemon python3 py3-pip build-base
+sudo apt update
+sudo apt install --no-cache bash git curl nodejs npm python3 py3-pip build-essential
+sudo npm install -g mocha chai nodemon
+
+
 
 # Use pip without cache to avoid extra layer size
 PY_PKGS="pytest pytest-html pytest-cov pylint"
