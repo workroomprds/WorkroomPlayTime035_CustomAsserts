448c0c13c2917cdcd4306b12f3c1d890b73f9898
James Lyndsay <jdl@workroom-productions.com>
2025-10-30 10:27:51 +0000

    Fix generated code – used poor name, delivered via XML, not as a string.

--- python-project/tests/test_MT103_gen.py
+++ python-project/tests/test_MT103_gen.py
@@ -1,13 +1,13 @@
 import xml.etree.ElementTree as ET
-from MT103_gen import generate_swift_payment
+from MT103_gen import generateMT103
 
 
 class TestMT103Gen:
     def test_should_export_a_generate_swift_payment_function(self):
-        assert callable(generate_swift_payment)
+        assert callable(generateMT103)
 
     def test_should_generate_valid_xml(self):
-        result = generate_swift_payment()
+        result = generateMT103()
         assert isinstance(result, ET.ElementTree)
         
         root = result.getroot()
@@ -17,7 +17,7 @@ class TestMT103Gen:
         ET.fromstring(xml_string)
 
     def test_xml_should_be_labelled_a_swift_payment_record(self):
-        result = generate_swift_payment()
+        result = generateMT103()
         root = result.getroot()
         
         assert root.tag == 'SwiftPayment'
